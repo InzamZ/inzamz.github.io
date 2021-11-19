82d0cfc23e28c590d52030ea405c3803c502802d
InzamZ <zhengyinzhen2001@icloud.com>
2021-11-19 13:55:07 +0800

    :pencil: update content

--- update.sh
+++ update.sh
@@ -4,6 +4,9 @@ if ["$info1" = ""]:
 then info1=":pencil: update content"
 fi
 
+export https_proxy="http://localhost:7890"
+export http_proxy="http://localhost:7890"
+
 git add -A
 git commit -m "$info1"
 git push origin hexo
