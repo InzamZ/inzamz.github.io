4079fc5f776d9c1b984db42904e393c609477846
InzamZ <zhengyinzhen2001@icloud.com>
2022-03-12 21:17:24 +0800

    :pencil: update content

--- update.sh
+++ update.sh
@@ -1,12 +1,7 @@
-info1=$1
-
-if ["$info1" = ""]:
-then info1=":pencil: update content"
+info=$1
+if ["$info" = ""];
+then info=":pencil: update content"
 fi
-
-export https_proxy="http://localhost:7890"
-export http_proxy="http://localhost:7890"
-
 git add -A
-git commit -m "$info1"
+git commit -m "$info"
 git push origin hexo
